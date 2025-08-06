95fbcd3208c46f6ee47293646908173fced68b8b
Donny Smith <donnysmith@Donnys-MacBook-Pro.local>
2025-08-06 11:31:39 -0600

    Add dark yellow favicon (#d4aa00 dot)

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning className={`${styreneA.variable} ${styreneB.variable} ${tiempos.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
